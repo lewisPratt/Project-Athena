94b6ab9402700878a48c032d3e29d4f8c5efd5a1
spikereloaded <lewis.198804@gmail.com>
2026-06-14 23:59:27 +0100

    added extra hover effects on gallery images

--- assets/js/gallery.js
+++ assets/js/gallery.js
@@ -7,8 +7,31 @@ document.querySelectorAll(".gallery-image").forEach(image => {
     });
 });
 
-document.querySelector("#lightbox").addEventListener("click", () => {
+// Close lightbox when clicking outside the image
+document.querySelector("#lightbox").addEventListener("click", (event) => {
+    if (event.target === document.querySelector("#lightbox")) {
+        closeLightbox();
+    }
+});
+
+window.addEventListener("keydown", (event) => {
+    if (event.key === "Escape") {
+        const lightbox = document.querySelector("#lightbox");
+        closeLightbox();
+    }
+});
+   
+
+window.addEventListener("scroll", () => {
+    const lightbox = document.querySelector("#lightbox");
+    if (!lightbox.classList.contains("hidden")) {
+        closeLightbox();
+    }
+});
+
+function closeLightbox() {
     const lightbox = document.querySelector("#lightbox");
     lightbox.classList.add("hidden");
+    lightbox.classList.remove("gallery-lightbox-flex");
     lightbox.innerHTML = "";
-});
+}
